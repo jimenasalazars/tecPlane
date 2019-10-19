ec59bd598366c0ed062be1267a9cee86201de0e9
Usuario <Usuario@172.19.54.39>
2019-10-19 10:59:29 -0600

    Timer en API

--- src/main/java/Datos/API.java
+++ src/main/java/Datos/API.java
@@ -5,10 +5,25 @@
  */
 package Datos;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author Usuario
  */
 public class API {
+        public static void main (String[] args){
+        Timer timer = new Timer();
         
+        TimerTask task= new TimerTask() {
+            @Override
+            public void run() {
+               System.out.println("Holaaaaaaa");
+            }
+        };{
+        
+    }
+        timer.schedule(task, 5000, 2000);
+    }
 }
